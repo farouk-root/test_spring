c23aa2407b6e645c46633b73a3ae0d88f5654361
farouk-root <farouk.chalghoumi031@gmail.com>
2024-04-30 11:40:21 +0200

    init

---
+++ src/test/java/tn/esprit/test_farouk/TestFaroukApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.test_farouk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFaroukApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
